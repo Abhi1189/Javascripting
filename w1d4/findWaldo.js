b00b35d63d1154654eb5223747c713344f2858c5
Abhi_G <devabhi.code@gmail.com>
2018-10-25 20:17:13 +0000

    Replaced the FOR loop with a .forEach function

--- w1d4/findWaldo.js
+++ w1d4/findWaldo.js
@@ -1,9 +1,14 @@
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] === "Waldo") {
+  //     found(i);   // execute callback
+  //   }
+  // }
+  arr.forEach(function(element, i){
+    if(element === "Waldo"){
+      found(i);
     }
-  }
+  });
 }
 
 function actionWhenFound(n) {
